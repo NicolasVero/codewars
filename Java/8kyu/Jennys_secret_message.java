c74088ba78f9467a51bb647d99fb6383c6ed1a41
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-08-10 00:42:37 +0200

    ajout exercices

---
+++ Java/8kyu/Jennys_secret_message.java
@@ -0,0 +1,6 @@
+public class Jennys_secret_message {
+    public static String greet(String name) {
+    
+        return (name.equals("Johnny")) ? "Hello, my love!" : String.format("Hello, %s!", name);
+    }
+}
