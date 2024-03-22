b55008ba429882588543acbb42c4166616def8ca
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2024-03-22 21:05:23 +0100

    ajout exercice

---
+++ Java/7kyu/More_than_Zero.java
@@ -0,0 +1,9 @@
+// Correct this code so that it takes one argument, x, and returns "x is more than zero" if x is positive (and nonzero), and otherwise, returns "x is equal to or less than zero." In both cases, replace x with the actual value of x.
+
+
+class More_than_Zero {
+    public static String corrections(int x) {
+      
+      return (x > 0) ? x + " is more than zero." : x + " is equal to or less than zero.";
+    }  
+}
