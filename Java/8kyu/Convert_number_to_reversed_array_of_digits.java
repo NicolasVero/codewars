3ee83080f586709a7682100e25547e9ad30b1883
Nicolas Vero <nicolasvero03@gmail.com>
2024-04-09 23:26:04 +0200

    ajout exercice

--- Java/8kyu/Convert_number_to_reversed_array_of_digits.java
+++ Java/8kyu/Convert_number_to_reversed_array_of_digits.java
@@ -1,3 +1,11 @@
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example(Input => Output):
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+
+
 public class Convert_number_to_reversed_array_of_digits {
 
     public static int[] digitize(long n) {
