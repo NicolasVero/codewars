94524733f2933cf32a43212b35c9ac18c735ae8d
Nicolas Vero <nicolasvero03@gmail.com>
2024-04-09 23:24:05 +0200

    ajout exercice

---
+++ Java/8kyu/Convert_number_to_reversed_array_of_digits.java
@@ -0,0 +1,14 @@
+public class Convert_number_to_reversed_array_of_digits {
+
+    public static int[] digitize(long n) {
+        final int strLength = String.valueOf(n).length();
+        int[] result = new int[strLength];
+
+        for(int i = 0; i < strLength; i++) {
+            result[i] = (int)(n % 10);
+            n /= 10; 
+        }
+
+        return result;
+    }
+}
