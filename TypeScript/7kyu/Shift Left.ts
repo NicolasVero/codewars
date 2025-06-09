9f9298d0d097e60f1644b91e5e80d9065a9c0787
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2025-06-09 23:36:33 +0200

    7kyu : Shift Left

---
+++ TypeScript/7kyu/Shift Left.ts
@@ -0,0 +1,26 @@
+// You are given two strings. In a single move, you can choose any of them, and delete the first (i.e. leftmost) character.
+
+// For Example:
+
+// By applying a move to the string "where", the result is the string "here".
+// By applying a move to the string "a", the result is an empty string "".
+// Implement a function that calculates the minimum number of moves that should be performed to make the given strings equal.
+
+// Notes
+// Both strings consist of lowercase latin letters.
+// If the string is already empty, you cannot perform any more delete operations.
+
+
+function shiftLeft(s, t) {
+    let i = s.length - 1;
+    let j = t.length - 1;
+    let common_length = 0;
+
+    while (i >= 0 && j >= 0 && s[i] === t[j]) {
+        common_length++;
+        i--;
+        j--;
+    }
+
+    return (s.length - common_length) + (t.length - common_length);
+}
