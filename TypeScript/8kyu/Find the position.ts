7241855afe7633f31a4459af4f096fcfd7533644
Nicolas <nicolasvero03@gmail.com>
2025-10-11 17:46:24 +0200

    8kyu : Find the position!

---
+++ TypeScript/8kyu/Find the position.ts
@@ -0,0 +1,12 @@
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Output :: "Position of alphabet: 1"
+
+// Note: Only lowercased English letters are tested
+
+
+export function position(alphabet: string): string {
+    return "Position of alphabet: " + (alphabet.charCodeAt(0) - 96);
+}
